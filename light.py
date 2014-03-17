fd928fc1bd1f9169316c2fd1968cdc816b352120
Scott Tomaszewski <scott.tomaszewski@lab49.com>
2014-03-17 09:55:37 -0400

    lighting basics

---
+++ light.py
@@ -0,0 +1,22 @@
+Class light:
+  def name(self):
+    return self.name
+
+  
+  
+  #TODO
+  def is_on(self):
+    return true
+
+  #TODO
+  def turn_on(self):
+    return 
+
+  #TODO
+  def turn_off(self):
+    return 
+
+  def __init__(name, daily_time_on, daily_time_off):
+    self.name = name
+    self.daily_time_on = daily_time_on
+    self.daily_time_off = daily_time_off
