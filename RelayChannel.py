c7c570d6482f54c74fc9debbf27cd63b8e6cc8a9
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-03-22 13:46:34 +0000

    Replaces manual gpio manipulation with RPi.GPIO

--- RelayChannel.py
+++ RelayChannel.py
@@ -1,29 +1,22 @@
+import RPi.GPIO as GPIO
 class RelayChannel:
   def on(self):
-    with open(self.dir + "value", "w") as f:
-      f.write("0")
+    GPIO.output(self.pin, GPIO.HIGH)
 
   def off(self):
-    with open(self.dir + "value", "w") as f:
-      f.write("1")
+    GPIO.output(self.pin, GPIO.LOW)
 
   def status(self):
-    with open(self.dir + "value", "r") as f:
-      return f.read()
+    return 0
 
   def setup(self):
     if str(self.pin).isdigit():
-      # Enable pin access via the Kernel on path '/sys/class/gpio/'
-      with open("/sys/class/gpio/export","a") as f:
-        f.write(str(self.pin))
-
-      # configure as output
-      with open(self.dir + "direction","w") as f:
-        f.write('out')
+      GPIO.setmode(GPIO.BOARD)
+      GPIO.setwarnings(False) 
+      GPIO.setup(self.pin, GPIO.OUT)
     else:
       raise Exception('Invalid pin: ' + str(self.pin))
 
   def __init__(self, gpioPin):
     self.pin = gpioPin
-    self.dir = "/sys/class/gpio/gpio" + str(self.pin) + "/"
     self.setup()
--- TestRelayChannel.py
+++ TestRelayChannel.py
@@ -1,5 +1,8 @@
 import RelayChannel
+import time
 
 if __name__ == "__main__":
-  toTest = RelayChannel.RelayChannel(17)
+  toTest = RelayChannel.RelayChannel(11)
   toTest.off()
+  time.sleep(2)
+  toTest.on()
