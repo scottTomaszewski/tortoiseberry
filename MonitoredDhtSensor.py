6b6b32d5c6ec7c1c323136996427027c74bec0ba
Scott Tomaszewski <scott.tomaszewski@lab49.com>
2014-03-30 00:54:26 -0400

    Adds wrapper for dht sensor

---
+++ MonitoredDhtSensor.py
@@ -0,0 +1,65 @@
+import subprocess
+import DhtSensor
+
+class DhtSensor:
+  def name(self):
+    return self.name
+  
+  def expected_temperature(self):
+    return self.expected_temp
+
+  def absolute_temperature(self):
+    return self.absolute_temp
+
+  def expected_humidity(self):
+    return self.expected_humidity
+
+  def absolute_humidity(self):
+    return self.absolute_humidity
+
+  def humidity(self):
+    return -1
+
+  def temperature(self):
+    return -1
+
+  def is_within_expected_humidity(self):
+    return self.compare_to_expected_humidity() == 0
+
+  def compare_to_expected_humidity(self):
+    return self.expected_humidity.compare_to(self.humidity())
+
+  def is_within_absolute_humidity(self):
+    return self.compare_to_absolute_humidity() == 0
+
+  def compare_to_absolute_humidity(self):
+    return self.absolute_humidity.compare_to(self.humidity())
+
+  def is_within_expected_temp(self):
+    return self.compare_to_absolute_temp() == 0
+
+  def compare_to_expected_temp(self):
+    return self.expected_temps.compare_to(self.temperature())
+
+  def is_within_absolute_temp(self):
+    return self.compare_to_absolute_temp() == 0
+
+  def compare_to_absolute_temp(self):
+    return self.absolute_temps.compare_to(self.temperature())
+    
+  def __init__(self, name, pin, expected_temperatures, absolute_temperatures, expected_humidity, absolute_humidity):
+    self.name = name
+    self.expected_temps = expected_temperatures
+    self.absolute_temps = absolute_temperatures
+    self.expected_humidity = expected_humidity
+    self.absolute_humidity = absolute_humidity
+    self.sensor = DhtSensor.DhtSensor(pin)
+  
+  @classmethod
+  def withRanges(cls, name, pin, expected_temperatures, absolute_temperatures, expected_humidity, absolute_humidity):
+    return cls(name, expected_temperatures, absolute_temperatures, expected_humidity, absolute_humidity)
+
+  @classmethod
+  def withoutRanges(cls, name, pin):
+    fullRange = Range.Range(0, 100)
+    return cls("name", fullRange, fullRange, fullRange, fullRange)
