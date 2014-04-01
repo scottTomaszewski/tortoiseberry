7aae351005b51fed5716f5db9d238df8add4806c
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-01 17:38:37 -0400

    Adds basic server impl

---
+++ sensors.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+pageTitle = "Spike!"
+
+print "Content-type: text/html"
+print
+print "<title>" + pageTitle + "</title>"
+print "<h1>Spike!</h1>"
+print "<p>Status: </p>"
+print ""
