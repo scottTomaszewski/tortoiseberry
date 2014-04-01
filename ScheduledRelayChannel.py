c72a20c1e08a2d13dcca887571b6f16b1357c82a
Scott Tomaszewski <scott.tomaszewski@lab49.com>
2014-04-01 10:54:20 -0400

    Adds scheduling

--- ScheduledRelayChannel.py
+++ ScheduledRelayChannel.py
@@ -1,10 +1,16 @@
 import RelayChannel
+from apscheduler.scheduler import Scheduler
 
 class ScheduledRelayChannel:
   def __init__(self, relayChannel, schedule):
     self.channel = relayChannel
     self.schedule = schedule
     self.enabled = True
+    self.sched = Scheduler()
+    self.sched.start()
+    for task in schedule.tasks():
+      fun = on if task.action == 'on' else off
+      sched.add_cron_job(job_function, day=task.day, hour=task.hour, minute=task.minute)
 
   def on(self):
     return self.channel.on()
@@ -17,7 +23,4 @@ class ScheduledRelayChannel:
 
   def enabled(boolean)
     self.enabled = boolean
-
-  def nextScheduledTimeAndAction(self):
-    return None
       
