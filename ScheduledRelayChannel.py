f927d070d423c31656ae68f77eb1943ff21b8a1f
Scott Tomaszewski <scott.tomaszewski@lab49.com>
2014-04-01 10:23:19 -0400

    Allows disabling and enabling

--- ScheduledRelayChannel.py
+++ ScheduledRelayChannel.py
@@ -4,6 +4,7 @@ class ScheduledRelayChannel:
   def __init__(self, relayChannel, schedule):
     self.channel = relayChannel
     self.schedule = schedule
+    self.enabled = True
 
   def on(self):
     return self.channel.on()
@@ -14,6 +15,9 @@ class ScheduledRelayChannel:
   def status(self):
     return self.channel.status()
 
+  def enabled(boolean)
+    self.enabled = boolean
+
   def nextScheduledTimeAndAction(self):
     return None
-
+      
