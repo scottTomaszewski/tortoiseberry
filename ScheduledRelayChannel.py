9a1e426a0431f60db31b800198a9445fb3fc29c5
Scott Tomaszewski <scott.tomaszewski@lab49.com>
2014-04-01 09:59:03 -0400

    Removes reasoning code

--- ScheduledRelayChannel.py
+++ ScheduledRelayChannel.py
@@ -4,14 +4,11 @@ class ScheduledRelayChannel:
   def __init__(self, relayChannel, schedule):
     self.channel = relayChannel
     self.schedule = schedule
-    self.reasonForLastChange = "Never changed"
 
   def on(self):
-    self.manuallyChangedReason(self.status(), "ON")
     return self.channel.on()
 
   def off(self):
-    self.manuallyChangedReason(self.status(), "OFF")
     return self.channel.off()
 
   def status(self):
