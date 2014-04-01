858c3a6b2fbdac56d67f39d01874427d40195f4f
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-01 14:34:25 -0400

    Removes commented code

--- ScheduledRelayChannel.py
+++ ScheduledRelayChannel.py
@@ -1,7 +1,6 @@
 import FakeRelayChannel
 #import RelayChannel
 from apscheduler.scheduler import Scheduler
-import atexit
 
 class ScheduledRelayChannel:
   def __init__(self, relayChannel, schedule):
@@ -11,37 +10,10 @@ class ScheduledRelayChannel:
     self.sched = Scheduler()
     self.sched.daemonic = False
     self.sched.start()
-    atexit.register(lambda: self.sched.shutdown(wait=False))
     for task in schedule.tasks():
       fun = self.on if task.action == 'on' else self.off
       self.sched.add_cron_job(fun, day=task.day, hour=task.hour, minute=task.minute)
     
- #   test_log = open('daemon.log', 'w')
- #   daemon.DaemonContext.files_preserve = [test_log]
-
- #   try:
- #     with daemon.DaemonContext():
- #       from datetime import datetime
- #       from apscheduler.scheduler import Scheduler
- #       import signal
- #
- #       logging.basicConfig(level=logging.DEBUG)
- #       sched = init_schedule()
- #
- #       print schedule.tasks()
- #       for task in schedule.tasks():
- #         fun = self.on if task.action == 'on' else self.off
- #         self.sched.add_cron_job(fun, day=task.day, hour=task.hour, minute=task.minute)
- #       signal.pause()
- #   except Exception, e:
- #     print e
- 
- # def init_schedule():
- #   sched = Scheduler()
- #   sched.standalone = True
- #   sched.start()
- #   return sched
-
   def on(self):
     return self.channel.on()
 
