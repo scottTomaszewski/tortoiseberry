543ba850f947fe9ea53ac89987ba3e53453544d0
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-06 01:05:33 -0400

    Begins integrating range into python cgi

--- StatusPage.py
+++ StatusPage.py
@@ -2,6 +2,7 @@ import RelayChannel
 import ScheduledRelayChannel
 import Schedule
 from string import Template
+import math
 
 #temp
 import datetime
@@ -25,9 +26,33 @@ class StatusPage:
     if 'overheadOff' in form:
       self.overhead.off()
 
+  def rangeHtml(self, title, min, max, value):
+    html = """
+      <div class='rangeContainer'>
+        <div class='rangeTitle'>T</div>
+        <div class='rangeBar blueRedRange'>
+    """
+    perc = (value-min) / (max-min)
+    mark = math.floor(perc * 39)
+    for i in range(1..39):
+      if i == mark-1 or i == mark+1:
+        html += "<div class='rangeTick rangeTickMed'></div>"
+      elif i == mark:
+        html += "<div class='rangeTick rangeTickLarge'>"
+        html += "  <div class='rangeTickValue'>" + value + "</div>"
+        html += "</div>"
+      else:
+        html += "<div class='rangeTick'></div>"
+    html += """
+      </div>
+    </div> 
+    """
+    return html
+
   def content(self):
     vars = {}
     vars['status'] = self.overhead.status()
+    vars['fooRange'] = self.rangeHtml()
     content = ""
     html = open('StatusPage.html','rb')
     for line in html:
