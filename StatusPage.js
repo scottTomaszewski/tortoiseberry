c99fdeff2dde2e7f4f241f7f2f42418bce58bb49
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-06 00:46:21 -0400

    Adds missing js

---
+++ StatusPage.js
@@ -0,0 +1,23 @@
+function startTime() {
+  var today=new Date();
+  var h=today.getHours();
+  var m=today.getMinutes();
+  var s=today.getSeconds();
+  h = h > 12 ? h-12 : h;
+  h = h == 0 ? 12 : h;
+  // add a zero in front of numbers < 10
+  m=checkTime(m);
+  s=checkTime(s);
+  document.getElementById('timeHourMinutes').innerHTML=h+":"+m;
+  document.getElementById('timeSeconds').innerHTML=s;
+  t=setTimeout(function(){startTime()},500);
+}
+ 
+function foo() { alert("bar"); }
+
+function checkTime(i) {
+  if (i<10) {
+    i="0" + i;
+  }
+  return i;
+}
