2ddfc4f878f82b53ed5db051180896935d94e150
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-10 15:40:44 -0400

    Adds date functionality

--- StatusPage.js
+++ StatusPage.js
@@ -1,10 +1,14 @@
 var refreshEnabled = false;
+var monthNames = [ "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December" ];
 
 function startTime() {
   var today=new Date();
   var h=today.getHours();
   var m=today.getMinutes();
   var s=today.getSeconds();
+  var mth=monthNames[today.getMonth()]
+  var day=today.getDay()
   if (s % 30 == 0 && refreshEnabled) { location.reload() }
   h = h > 12 ? h-12 : h;
   h = h == 0 ? 12 : h;
@@ -13,6 +17,7 @@ function startTime() {
   s=checkTime(s);
   document.getElementById('timeHourMinutes').innerHTML=h+":"+m;
   document.getElementById('timeSeconds').innerHTML=s;
+  document.getElementById('dateContainer').innerHTML=mth+" "+day;
   t=setTimeout(function(){ startTime() },500);
 }
 
