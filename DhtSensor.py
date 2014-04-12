73431227f25123de8bc21ca9d8c7af1f03c31455
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-12 02:11:07 +0000

    Backgrounds dht update to not hang up page refresh

--- DhtSensor.py
+++ DhtSensor.py
@@ -1,16 +1,16 @@
 import subprocess
-import time
+import threading
 
 # The DHT sensor only updates every 2 seocnds which is the reasoning for the caching of read values
 class DhtSensor:
   def humidity(self):
-    out = self.dht()
+    out = self.lastRead
     if "Hum" in out:
       return int(float(out[out.index("Hum"):].split()[2]))
     return -1
 
   def temperature(self):
-    out = self.dht()
+    out = self.lastRead
     if "Temp" in out:
       return float(out[out.index("Temp"):].split()[2])
     return -1
@@ -18,19 +18,18 @@ class DhtSensor:
   def temperatureF(self):
     return int(float(9.0/5.0 * self.temperature() + 32))
 
-  def dht(self):
-    return self.dhtTimeout(3)
-
   def dhtTimeout(self, count):
-    # refresh only if 2 seconds has expired
-    if (self.lastReadTime == None or time.time() - self.lastReadTime > 5 or 'Temp' not in self.lastRead):
-      p = subprocess.Popen(["sudo", "./Adafruit-Raspberry-Pi-Python-Code/Adafruit_DHT_Driver/Adafruit_DHT", str(self.model), str(self.pin)], stdout=subprocess.PIPE)
-      self.lastRead = p.communicate()[0]
-      self.lastReadTime = time.time()
-      print self.lastRead
-    if 'Temp' not in self.lastRead:
-      return self.dhtTimeout(count-1)
-    return self.lastRead
+    p = subprocess.Popen(["sudo", "./Adafruit-Raspberry-Pi-Python-Code/Adafruit_DHT_Driver/Adafruit_DHT", str(self.model), str(self.pin)], stdout=subprocess.PIPE)
+    maybe = p.communicate()[0]
+    print self.lastRead
+    if 'Temp' not in maybe:
+      self.dhtTimeout(count-1)
+    else:
+      self.lastRead = maybe
+
+  def update(self): 
+    threading.Timer(10, self.update).start(); 
+    return self.dhtTimeout(3)
 
   def __init__(self, pin, model):
     if not str(pin).isdigit():
@@ -38,5 +37,4 @@ class DhtSensor:
     self.pin = pin
     self.model = model
     self.lastRead = None
-    self.lastReadTime = None
-    self.dht()
+    self.update()
