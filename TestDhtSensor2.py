9186306b3b7b6916dd6719d4b8e85db49c571e24
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-25 13:57:48 +0000

    Removes unneeded file

--- TestDhtSensor2.py
+++
@@ -1,6 +0,0 @@
-import DhtSensor2
-
-if __name__ == "__main__":
-  toTest = DhtSensor2.DhtSensor2(4)
-  print "Humidity: " + str(toTest.humidity()) + "%"
-  print "Temperature: " + str(toTest.temperature()) + "c"
