5334bfcbc29cb538ba09b30d14a865130f36659e
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-04-28 10:16:19 -0400

    Renames class DHT22 -> DHT11

--- DHT11.py
+++ DHT11.py
@@ -4,10 +4,10 @@ import time
 
 import pigpio
 
-class DHT22_sensor:
+class DHT11_sensor:
    """
    A class to read relative humidity and temperature from the
-   DHT22 sensor.  The sensor is also known as the AM2302.
+   DHT11 sensor.  The sensor is also known as the AM2302.
 
    The sensor can be powered from the Pi 3V3 or the Pi 5V rail.
 
@@ -24,14 +24,14 @@ class DHT22_sensor:
 
    5V--5K_resistor--+--10K_resistor--Ground
                     |
-   DHT22 pin 2 -----+
+   DHT11 pin 2 -----+
                     |
    gpio ------------+
    """
 
    def __init__(self, gpio):
       """
-      Instantiate with the gpio to which the DHT22 output pin is connected.
+      Instantiate with the gpio to which the DHT11 output pin is connected.
       """
 
       self.gpio = gpio
@@ -175,7 +175,7 @@ class DHT22_sensor:
       pigpio.set_watchdog(self.gpio, 50)
 
    def cancel(self):
-      """Cancel the DHT22 sensor."""
+      """Cancel the DHT11 sensor."""
       self.cb.cancel()
 
 if __name__ == "__main__":
@@ -184,11 +184,11 @@ if __name__ == "__main__":
 
    import pigpio
 
-   import DHT22
+   import DHT11
 
    pigpio.start()
 
-   s = DHT22.DHT22_sensor(7)
+   s = DHT11.DHT11_sensor(7)
 
    r = 0
 
