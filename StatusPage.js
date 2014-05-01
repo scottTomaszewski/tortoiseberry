0fee1d2c84e060c27f6458330e9575e6add1c0c8
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-01 17:44:12 +0000

    Adds counter of last update

--- StatusPage.js
+++ StatusPage.js
@@ -1,14 +1,18 @@
 var monthNames = [ "January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December" ];
+var refreshed = new Date();
 
 function startTime() {
+  dateTime();
+}
+
+function dateTime() {
   var today=new Date();
   var h=today.getHours();
   var m=today.getMinutes();
   var s=today.getSeconds();
   var mth=monthNames[today.getMonth()]
   var day=today.getDay()
-  if (s % 5 == 0) { refreshData() }
   h = h > 12 ? h-12 : h;
   h = h == 0 ? 12 : h;
   // add a zero in front of numbers < 10
@@ -17,7 +21,9 @@ function startTime() {
   document.getElementById('timeHourMinutes').innerHTML=h+":"+m;
   document.getElementById('timeSeconds').innerHTML=s;
   document.getElementById('dateContainer').innerHTML=mth+" "+day;
-  t=setTimeout(function(){ startTime() },500);
+  if (s % 5 == 0) { refreshData() }
+  $("#lastUpdated").html(Math.ceil((today - refreshed)/1000));
+  t=setTimeout(function(){ dateTime() },500);
 }
 
 function checkTime(i) {
@@ -29,6 +35,7 @@ function checkTime(i) {
 
 function refreshData() {
   $.getJSON( "/update", function( data ) {
+    refreshed = new Date();
     var items = [];
     $.each( data, function( key, val ) {
       if (key == 'topLeftTempValue') {
