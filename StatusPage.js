3468eabefec6054b48c3cbf1c2b0caa43abe2b16
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-01 17:20:09 +0000

    Removes refresh option

--- StatusPage.js
+++ StatusPage.js
@@ -1,4 +1,3 @@
-var refreshEnabled = true;
 var monthNames = [ "January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December" ];
 
@@ -9,7 +8,7 @@ function startTime() {
   var s=today.getSeconds();
   var mth=monthNames[today.getMonth()]
   var day=today.getDay()
-  if (s % 5 == 0 && refreshEnabled) { refreshData() }
+  if (s % 5 == 0) { refreshData() }
   h = h > 12 ? h-12 : h;
   h = h == 0 ? 12 : h;
   // add a zero in front of numbers < 10
@@ -19,14 +18,6 @@ function startTime() {
   document.getElementById('timeSeconds').innerHTML=s;
   document.getElementById('dateContainer').innerHTML=mth+" "+day;
   t=setTimeout(function(){ startTime() },500);
-}
-
-function enableRefresh() {
-  refreshEnabled = true;
-}
-
-function disableRefresh() {
-  refreshEnabled = false;
 }
 
 function checkTime(i) {
