56f8d923e1f2bb614d1885d3e155036de91ac913
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-02 11:30:10 -0400

    Removes unused code

--- TestDhtSensor.py
+++
@@ -1,6 +0,0 @@
-import DhtSensor
-
-if __name__ == "__main__":
-  toTest = DhtSensor.DhtSensor(4, '11')
-  print "Humidity: " + str(toTest.humidity()) + "%"
-  print "Temperature: " + str(toTest.temperature()) + "c"
