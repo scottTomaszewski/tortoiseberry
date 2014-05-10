9625f883a6fa7a033a4fa27b1f2014f9b316cd07
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-10 11:55:24 -0400

    Formatting

--- Range.py
+++ Range.py
@@ -1,20 +1,20 @@
 class Range:
-  def min(self):
-    return self.min
+    def min(self):
+        return self.min
 
-  def max(self):
-    return self.max
+    def max(self):
+        return self.max
 
-  def is_within_bounds(self, maybe):
-    return self.compare_to(maybe) == 0
+    def is_within_bounds(self, maybe):
+        return self.compare_to(maybe) == 0
 
-  def compare_to(self, toCheck):
-    if toCheck < self.get_min():
-      return -1
-    if toCheck > self.get_max():
-      return 1
-    return 0
+    def compare_to(self, toCheck):
+        if toCheck < self.get_min():
+            return -1
+        if toCheck > self.get_max():
+            return 1
+        return 0
 
-  def __init__(self, min, max):
-    self.min = min
-    self.max = max
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
