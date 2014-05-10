b028189a78af0d961be1193f0e5d0858e13de19b
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-10 11:56:48 -0400

    Avoids shadowing python

--- Range.py
+++ Range.py
@@ -15,6 +15,6 @@ class Range:
             return 1
         return 0
 
-    def __init__(self, min, max):
-        self.min = min
-        self.max = max
+    def __init__(self, minimum, maximum):
+        self.min = minimum
+        self.max = maximum
