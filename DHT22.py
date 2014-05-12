c3c8b249c1b7770702b87a8884838d896da39312
Scott Tomaszewski <scottTomaszewski@gmail.com>
2014-05-12 09:09:38 -0400

    Renames based on parents refactoring

--- DHT22.py
+++ DHT22.py
@@ -5,15 +5,15 @@ import pigpio
 
 class DHT22Sensor(DHT11):
     def adjust_temperature(self):
-        if self.tH & 128:  # negative temperature
+        if self.temp_high_byte & 128:  # negative temperature
             multiplier = -0.1
             self.temp_high_byte = self.temp_high_byte & 127
         else:
             multiplier = 0.1
-        return ((self.tH << 8) + self.tL) * multiplier
+        return ((self.temp_high_byte << 8) + self.temp_low_byte) * multiplier
 
     def adjust_humidity(self):
-        return ((self.hH << 8) + self.hL) * 0.1
+        return ((self.hum_high_byte << 8) + self.hum_low_byte) * 0.1
 
 
 if __name__ == "__main__":
